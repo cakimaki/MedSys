6470c784711cc400c02a7223c6ec60de39c59932
mdianaa <dani.tsakovski3@gmail.com>
2024-12-31 18:06:53 +0200

    Creation

--- src/main/java/org/example/medsys/entity/Patient.java
+++ src/main/java/org/example/medsys/entity/Patient.java
@@ -1,4 +1,22 @@
 package org.example.medsys.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Patient {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
